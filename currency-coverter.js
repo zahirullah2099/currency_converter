75e8f064b63c8014b6cef0939503a1fc9fc87e82
zahir ullah <zahirullah2099@gmail.com>
2023-12-30 22:44:05 +0500

    project of currency coverter
    project of currency coverter

---
+++ currency-coverter.js
@@ -0,0 +1,224 @@
+
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  }; 
+
+const apiURL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+let dropdowns = document.querySelectorAll(" select");
+const btn = document.querySelector("button");
+let fromCurr = document.querySelector(".from select")
+let toCurr = document.querySelector(".to select")
+let msg = document.querySelector(".msg");
+
+
+
+
+for (let select of dropdowns){ 
+    for(  country_codes in countryList){
+      let newOPtion = document.createElement("option");
+      newOPtion.innerText=country_codes;
+      // newOPtion.value= country_code;
+    if(select.name === "from" && country_codes.value ==="USD"){
+      newOPtion.selected="selected";
+    }else if(select.name === "to" && country_codes.value ==="PKR"){
+      newOPtion.selected="selected";
+
+    }
+      select.append(newOPtion); 
+    }
+    select.addEventListener("change", (evt)=>{
+      updateFlag(evt.target)
+    })
+}
+const updateExchangeRate =async () =>{
+  let amount = document.querySelector(".amount input");
+  let amountValue = amount.value; 
+  if(amountValue === "" || amountValue < 1){
+    amountValue=1;
+    amount.value="";
+  }
+
+ const URL =`${apiURL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+let response = await fetch(URL);
+let data = await response.json();
+let rate = data [toCurr.value.toLowerCase()];
+ let finalAmount = amountValue*rate;
+ msg.innerText = `${amountValue} ${fromCurr.value} = ${finalAmount}  ${toCurr.value}`
+}
+
+const updateFlag = (element)=>{
+  let country_codes = element.value;
+  // console.log(country_codes);
+  let codes = countryList[country_codes];
+  let newSrc = `https://flagsapi.com/${codes}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src=newSrc
+}
+ 
+btn.addEventListener("click",  (evt)=>{
+  evt.preventDefault();
+  updateExchangeRate();
+});
+ 
+window.addEventListener("load", ()=>{
+  updateExchangeRate();
+})
